0df59bd8d53814378abe6dc18f1e226f0989eccc
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2023-10-27 01:52:44 +0700

    format : clear unused component

--- src/components/Skeleton/SuperAdminAccess.tsx
+++ src/components/Skeleton/SuperAdminAccess.tsx
@@ -3,7 +3,7 @@ import Box from "@mui/material/Box";
 import SpeedDial from "@mui/material/SpeedDial";
 import SpeedDialIcon from "@mui/material/SpeedDialIcon";
 import SpeedDialAction from "@mui/material/SpeedDialAction";
-import { AccountTree, PeopleAlt } from "@mui/icons-material";
+import { AccountTree } from "@mui/icons-material";
 import { ListRequestFamily } from "../Auth/ListRequestFamily";
 import { Node } from "../../types/family.type";
 
