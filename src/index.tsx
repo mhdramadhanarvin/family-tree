5e65855a7f93bf1874bf08526bf8f6711ce4f7a7
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2023-10-30 03:23:08 +0700

    feat : ui with handling data add global family

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import "./index.css";
 
-import React from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
